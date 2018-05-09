cba8daa27f7d87a00d3fcb6555543aebe37ed350
André <Andr@DESKTOP-QR1JGFI>
2018-05-09 17:11:08 -0300

    Testtes na classe usuario

---
+++ src/java/br/com/fatepg/quiz/Usuario.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.fatepg.quiz;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+
+/**
+ *
+ * @author André
+ */
+public class Usuario {
+    
+    private String nome;
+    private double media;
+    List<Double> last10 = new ArrayList<Double>();
+    
+    
+    
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getMedia() {
+        return media;
+    }
+
+    public double setMedia(Object last10) {
+        double total;
+        
+        
+        
+       return media;
+       
+    }
+
+    
+    
+    
+    
+}
